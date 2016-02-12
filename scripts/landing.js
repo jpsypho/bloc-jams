43cdefd62372164cf8c7805cb39427da9c6cd8ae
Jim <jim@getroionline.com>
2016-02-11 22:42:41 -0600

    checkpoint-23-events-completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,8 +1,9 @@
-var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+	var animatePoints = function(points) {
  
      var revealPoint = function(index) {
+         // points[0].style.opacity = 1;
          points[index].style.opacity = 1;
          points[index].style.transform = "scaleX(1) translateY(0)";
          points[index].style.msTransform = "scaleX(1) translateY(0)";
@@ -14,4 +15,14 @@ var animatePoints = function() {
     
  };
 
-animatePoints();
+window.onload = function() {
+	var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+	
+	window.addEventListener('scroll', function(event) {
+		if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+	});
+}
